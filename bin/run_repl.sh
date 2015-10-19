7dfc87f9d4359f31eb78d245e727f8ba47071f31
David Schmitz <david.schmitz.privat@gmail.com>
2015-10-19 23:12:41 +0200

    working on slides

--- bin/run_repl.sh
+++ bin/run_repl.sh
@@ -1,1 +1,1 @@
-docker run -ti --name ElixirREPL -v ~/ownCloud/_Vortraege/talks-elixir-intro/src/:/home/demo/src koenighotze/elixir 
+docker run -ti --name ElixirREPL -v ~/ownCloud/_Vortraege/talks-elixir-intro/demo-project/:/home/demo/src koenighotze/elixir 
