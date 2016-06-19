a5f63527f8c8cf70d447a07734dcd43a8f1016d1
David Schmitz <david.schmitz.privat@gmail.com>
2016-06-19 21:28:15 +0200

    finalized

--- demo/advanced/lib/agenttry.ex
+++ demo/advanced/lib/agenttry.ex
@@ -1,3 +1,13 @@
 defmodule AgentTry do
-  
+
+  def runagent do
+    {:ok, agent} = Agent.start_link fn -> %{} end
+
+    Agent.update(agent, fn current -> Map.put(current, :foo, "bar") end)
+
+    state = Agent.get(agent, fn s -> s end)
+
+    IO.puts "State is #{inspect state}"
+  end
+
 end
